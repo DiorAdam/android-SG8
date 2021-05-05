f61288d58c63ca0ed2504c3d67cc01c1acdfc93d
fadel <fadelsougou@gmail.com>
2021-05-06 00:32:52 +0200

    feat http requests to httpbin.org for authentication

---
+++ app/src/main/java/com/example/td2/Authentication.java
@@ -0,0 +1,59 @@
+package com.example.td2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Authentication extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceData){
+        super.onCreate(savedInstanceData);
+        this.setContentView(R.layout.authentication);
+
+        Button button_login = this.findViewById(R.id.button_login);
+        button_login.setOnClickListener(ev->{
+            new Thread(() -> {
+                URL url = null;
+                try {
+                    url = new URL("https://httpbin.org/basic-auth/bob/sympa");
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                    try {
+                        String user_name = ((EditText) findViewById(R.id.textfield_username)).getText().toString();
+                        String password = ((EditText) findViewById(R.id.textfield_password)).getText().toString();
+                        String basicAuth = "Basic " + Base64.encodeToString(
+                                (user_name + ":" + password).getBytes(),
+                                Base64.NO_WRAP);
+                        urlConnection.setRequestProperty ("Authorization", basicAuth);
+                        InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                        String s = Authentication.readStream(in);
+                        Log.i("yolo", s);
+                    } finally {
+                        urlConnection.disconnect();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        });
+    }
+
+    public static String readStream(InputStream is) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader r = new BufferedReader(new InputStreamReader(is),1000);
+        for (String line = r.readLine(); line != null; line =r.readLine()){
+            sb.append(line);
+        }
+        is.close();
+        return sb.toString();
+    }
+}
--- app/src/main/java/com/example/td2/MainActivity.java
+++ app/src/main/java/com/example/td2/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.td2;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent auth_call = new Intent(getApplicationContext(), Authentication.class);
+        startActivity(auth_call);
     }
 }
